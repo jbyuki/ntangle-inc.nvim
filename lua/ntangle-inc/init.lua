c4ce017187806d67035a0edf77799e51388b1739
Julien Burkhard <jyburkhard@hotmail.com>
2024-12-26 19:36:30 +0900

    fix lsp paths

--- lua/ntangle-inc/init.lua
+++ lua/ntangle-inc/init.lua
@@ -1696,7 +1696,7 @@ function M.start()
 					local hl_path = M.hl_to_hl_path[hl]
 					if hl_path then
 						local parent_path = vim.fs.dirname(hl_path)
-						local bufname = vim.fs.normalize(vim.fs.joinpath(parent_path, M.ntangle_folder))
+						local bufname = vim.fs.normalize(vim.fs.joinpath(parent_path, M.ntangle_folder, root_section.name))
 						local buf_path = util.path.sanitize(bufname)
 						local uri = vim.uri_from_fname(buf_path)
 						M.buf_to_uri[buf] = uri
